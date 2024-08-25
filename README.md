ee5f53aa043d7a5e8ba18d855ffc7cc92cb70c62
Aji Abdul Sajid <ajiabdulsajid@gmail.com>
2024-08-25 18:00:07 +0700

    Change code

--- README.md
+++ README.md
@@ -21,4 +21,25 @@ Original Plugins https://www.jqueryscript.net/form/cascading-selection-box.html
       console.log(value, label, datas);
     }
   });
+  // other option with class i use fontawesome
+  iconSelected:'fontawesome and other icons your like' //
+  iconPostfix:'fontawesome and other icons your like insert arrow-right class' // in this please add "arrow-right" or "icon-right" or yout adjusted with css otherwise the layout will be messy, find zd-cascader-node__postfix.
+  // Select Data By ID
+	$('#test').zdCascader('select', [2, 22, 222]);
+  // Open
+	$('#test').zdCascader('open');
+  // Close
+	$('#test').zdCascader('close');
+  // get id from search and by clicked
+  let id = ($.isArray(label.value)) ? label.value.slice(-1)[0] : label.value;
+  // get id from search
+  if ($.isArray(label.value)) {
+    let ids = datas[0].value;
+    // reconstruct data
+    $('#test').zdCascader('reload');
+    $('#test').zdCascader('select', ids);
+  } else {
+    // get id from select data
+    let ids = datas.map(item => item.id).sort();
+  }
 ```
--- languages.js
+++ languages.js
@@ -1,143 +1,143 @@
 // languages.js
 var languages = [
-    {
-      "indexcode": "1",
-      "name": "Front-End",
-      "s": [
-        {
-        "indexcode": "11",
-        "name": "HTML",
-          "s": [
-            {
-                "indexcode": "111",
-                "name": "HTML"
-            },
-            {
-                "indexcode": "112",
-                "name": "XML"
-            },
-            {
-                "indexcode": "113",
-                "name": "HTML5"
-            },
-          ]
-        },
-        {
-        "indexcode": "12",
-        "name": "CSS",
-          "s": [
-            {
-                "indexcode": "121",
-                "name": "CSS"
-            },
-            {
-                "indexcode": "122",
-                "name": "CSS3"
-            },
-            {
-                "indexcode": "123",
-                "name": "CSS4"
-            },
-          ]
-        },
-        {
-        "indexcode": "13",
-        "name": "JavaScript",
-          "s": [
-            {
-                "indexcode": "131",
-                "name": "jQuery"
-            },
-            {
-                "indexcode": "122",
-                "name": "Angular"
-            },
-            {
-                "indexcode": "123",
-                "name": "React"
-            },
-          ]
-        },
-      ]
-    },
+	{
+		"id": "1",
+		"name": "Front-End",
+		"children": [
+			{
+				"id": "11",
+				"name": "HTML",
+				"children": [
+					{
+						"id": "111",
+						"name": "HTML"
+					},
+					{
+						"id": "112",
+						"name": "XML"
+					},
+					{
+						"id": "113",
+						"name": "HTML5"
+					},
+				]
+			},
+			{
+				"id": "12",
+				"name": "CSS",
+				"children": [
+					{
+						"id": "121",
+						"name": "CSS"
+					},
+					{
+						"id": "122",
+						"name": "CSS3"
+					},
+					{
+						"id": "123",
+						"name": "CSS4"
+					},
+				]
+			},
+			{
+				"id": "13",
+				"name": "JavaScript",
+				"children": [
+					{
+						"id": "131",
+						"name": "jQuery"
+					},
+					{
+						"id": "122",
+						"name": "Angular"
+					},
+					{
+						"id": "123",
+						"name": "React"
+					},
+				]
+			},
+		]
+	},
 
-    {
-      "indexcode": "2",
-      "name": "Back-End",
-      "s": [
-        {
-        "indexcode": "21",
-        "name": "C",
-          "s": [
-            {
-                "indexcode": "211",
-                "name": "C"
-            },
-            {
-                "indexcode": "212",
-                "name": "C++"
-            },
-            {
-                "indexcode": "213",
-                "name": "C#"
-            },
-          ]
-        },
-        {
-        "indexcode": "22",
-        "name": "Database",
-          "s": [
-            {
-                "indexcode": "221",
-                "name": "MySql"
-            },
-            {
-                "indexcode": "222",
-                "name": "SQL"
-            },
-            {
-                "indexcode": "223",
-                "name": "Oracle"
-            },
-          ]
-        },
-        {
-        "indexcode": "23",
-        "name": "Others",
-          "s": [
-            {
-                "indexcode": "231",
-                "name": "Node.js"
-            },
-            {
-                "indexcode": "122",
-                "name": "Python"
-            },
-            {
-                "indexcode": "123",
-                "name": "Ruby"
-            },
-          ]
-        },
-        // feel free to add your own data to the dropdown
-        {
-        "indexcode": "24",
-        "name": "jQueryScript",
-          "s": [
-            {
-                "indexcode": "241",
-                "name": "jQueryScript"
-            },
-            {
-                "indexcode": "242",
-                "name": "CSSScript"
-            },
-            {
-                "indexcode": "243",
-                "name": "VueScript"
-            },
-          ]
-        },
-      ]
-    }
+	{
+		"id": "2",
+		"name": "Back-End",
+		"children": [
+			{
+				"id": "21",
+				"name": "C",
+				"children": [
+					{
+						"id": "211",
+						"name": "C"
+					},
+					{
+						"id": "212",
+						"name": "C++"
+					},
+					{
+						"id": "213",
+						"name": "C#"
+					},
+				]
+			},
+			{
+				"id": "22",
+				"name": "Database",
+				"children": [
+					{
+						"id": "221",
+						"name": "MySql"
+					},
+					{
+						"id": "222",
+						"name": "SQL"
+					},
+					{
+						"id": "223",
+						"name": "Oracle"
+					},
+				]
+			},
+			{
+				"id": "23",
+				"name": "Others",
+				"children": [
+					{
+						"id": "231",
+						"name": "Node.js"
+					},
+					{
+						"id": "122",
+						"name": "Python"
+					},
+					{
+						"id": "123",
+						"name": "Ruby"
+					},
+				]
+			},
+			// feel free to add your own data to the dropdown
+			{
+				"id": "24",
+				"name": "jQueryScript",
+				"children": [
+					{
+						"id": "241",
+						"name": "jQueryScript"
+					},
+					{
+						"id": "242",
+						"name": "CSSScript"
+					},
+					{
+						"id": "243",
+						"name": "VueScript"
+					},
+				]
+			},
+		]
+	}
 ]
